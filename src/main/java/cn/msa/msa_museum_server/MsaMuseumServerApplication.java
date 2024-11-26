06087e0a402b5c1edb4cd2f2e999a1a4ec59ba1c
Hikari <xuanzhao@moonshotacademy.cn>
2024-11-26 15:38:44 +0800

    project init

---
+++ src/main/java/cn/msa/msa_museum_server/MsaMuseumServerApplication.java
@@ -0,0 +1,13 @@
+package cn.msa.msa_museum_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaMuseumServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaMuseumServerApplication.class, args);
+	}
+
+}
